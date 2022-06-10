569a71db29211a3485a9a8e8f03780c0b67db270
Guillermo Fidalgo <guillermo.fidalgo@upr.edu>
2022-06-10 12:25:22 -0400

    made a new function to print author of changes

--- src/planetsmath/functions.py
+++ src/planetsmath/functions.py
@@ -28,3 +28,9 @@ def sum_product(list):
     for item in list:
         product *= item
     return product
+
+
+def make_author_print(author="Guillermo"):
+	"""This function makes a print of the author of this change""" 
+	print("Guillermo Made a fork")
+
